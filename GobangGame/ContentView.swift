5f24afd835fb165940cb7c10d1e2af16faefafbf
xuzl-d <69352418+xuzl-d@users.noreply.github.com>
2026-03-22 17:08:26 +0800

    Initial Commit

---
+++ GobangGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GobangGame
+//
+//  Created by xuzhilin on 2026/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GobangGame/GobangGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GobangGameApp.swift
+//  GobangGame
+//
+//  Created by xuzhilin on 2026/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GobangGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
